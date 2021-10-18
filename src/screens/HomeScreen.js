b0ab71b7c884206e6307a2feda4beb748c92a5dc
Rokas <rookas.rudzenskas@gmail.com>
2021-10-18 16:45:09 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -19,78 +19,82 @@ const HomeScreen = () => {
     const [me, setMe] = useState(null);
     const [users, setUsers] = useState([]);
 
+    // useEffect(() => {
+    //     const getCurrentUser = async () => {
+    //         const user = await Auth.currentAuthenticatedUser();
+    //
+    //         const dbUsers = await DataStore.query(
+    //             User,
+    //             u => u.sub === user.attributes.sub,
+    //         );
+    //         if (dbUsers.length < 0) {
+    //             return;
+    //         }
+    //         setMe(dbUsers[0]);
+    //     };
+    //     getCurrentUser().then();
+    // }, []);
+    //
+    //
+
+    const fetchUsers = async () => {
+        const fetchedUsers = await DataStore.query(User);
+        console.log("This is users", fetchedUsers);
+    };
+
     useEffect(() => {
-        const getCurrentUser = async () => {
-            const user = await Auth.currentAuthenticatedUser();
-
-            const dbUsers = await DataStore.query(
-                User,
-                u => u.sub === user.attributes.sub,
-            );
-            if (dbUsers.length < 0) {
-                return;
-            }
-            setMe(dbUsers[0]);
-        };
-        getCurrentUser().then();
-    }, []);
-
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const fetchedUsers = await DataStore.query(User);
-            setUsers(fetchedUsers);
-        };
         fetchUsers();
     }, []);
 
-    const onSwipeLeft = () => {
-        if(!currentUser || !me) {
-            return;
-        }
-        console.warn('Swiped Left', currentUser?.name);
-    }
-
-    const onSwipeRight = async () => {
-        if (!currentUser || !me) {
-            return;
-        }
-
-        const myMatches = await DataStore.query(Match, match =>
-            match.User1ID('eq', me.id).User2ID('eq', currentUser.id),
-        );
-        if (myMatches.length > 0) {
-            console.warn('You already swiped right to this user');
-            return;
-        }
-
-        const hisMatches = await DataStore.query(Match, match =>
-            match.User1ID('eq', currentUser.id).User2ID('eq', me.id),
-        );
-
-        console.log('hisMatches');
-        console.log('User1 ', currentUser.id);
-        console.log('User2 ', me.id);
-        console.log(hisMatches);
-
-        if (hisMatches.length > 0) {
-            console.log('Yay, this is a new match');
-            const hisMatch = hisMatches[0];
-            DataStore.save(
-                Match.copyOf(hisMatch, updated => (updated.isMatch = true)),
-            ).then();
-            return;
-        }
-
-        console.warn('Sending him a match request!');
-        const newMatch = new Match({
-            User1ID: me.id,
-            User2ID: currentUser.id,
-            isMatch: false,
-        });
-        console.log(newMatch);
-        DataStore.save(newMatch).then();
-    };
+    console.log(users);
+    //
+    // const onSwipeLeft = () => {
+    //     if(!currentUser || !me) {
+    //         return;
+    //     }
+    //     console.warn('Swiped Left', currentUser?.name);
+    // }
+    //
+    // const onSwipeRight = async () => {
+    //     if (!currentUser || !me) {
+    //         return;
+    //     }
+    //
+    //     const myMatches = await DataStore.query(Match, match =>
+    //         match.User1ID('eq', me.id).User2ID('eq', currentUser.id),
+    //     );
+    //     if (myMatches.length > 0) {
+    //         console.warn('You already swiped right to this user');
+    //         return;
+    //     }
+    //
+    //     const hisMatches = await DataStore.query(Match, match =>
+    //         match.User1ID('eq', currentUser.id).User2ID('eq', me.id),
+    //     );
+    //
+    //     console.log('hisMatches');
+    //     console.log('User1 ', currentUser.id);
+    //     console.log('User2 ', me.id);
+    //     console.log(hisMatches);
+    //
+    //     if (hisMatches.length > 0) {
+    //         console.log('Yay, this is a new match');
+    //         const hisMatch = hisMatches[0];
+    //         DataStore.save(
+    //             Match.copyOf(hisMatch, updated => (updated.isMatch = true)),
+    //         ).then();
+    //         return;
+    //     }
+    //
+    //     console.warn('Sending him a match request!');
+    //     const newMatch = new Match({
+    //         User1ID: me.id,
+    //         User2ID: currentUser.id,
+    //         isMatch: false,
+    //     });
+    //     console.log(newMatch);
+    //     DataStore.save(newMatch).then();
+    // };
 
 
 
