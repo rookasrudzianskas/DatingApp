17a23b30f6877a5bb2276a7dd391206633a851c9
Rokas <rookas.rudzenskas@gmail.com>
2021-10-24 10:24:27 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -63,7 +63,6 @@ const HomeScreen = ({isUserLoading}) => {
     // -----------------------------------------------------------------------------------------
 
     useEffect(() => {
-        // console.log(me);
         if (isUserLoading || !me || matchesIds === null) {
             return;
         }
@@ -72,7 +71,8 @@ const HomeScreen = ({isUserLoading}) => {
                 user.gender('eq', me.lookingFor),
             );
 
-            fetchedUsers = fetchUsers.filter(u => !matchesIds.includes(u.id));
+            fetchedUsers = fetchedUsers.filter(u => !matchesIds.includes(u.id));
+
             setUsers(fetchedUsers);
         };
         fetchUsers();
