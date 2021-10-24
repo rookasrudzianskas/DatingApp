fd860e90600d1c046b958299f2d63ff6abb5eb10
Rokas <rookas.rudzenskas@gmail.com>
2021-10-24 10:12:59 +0300

    Rokas

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -52,6 +52,7 @@ const HomeScreen = ({isUserLoading}) => {
             // console.log(me.id);
             const result = await DataStore.query(Match, m => m.isMatch('eq', true).or(m1 => m1.User1ID('eq', me.id).User2ID('eq', me.id)),);
             // console.log("This is result about the users", result);
+            console.log("This is result", result);
             setMatches(result);
         };
         fetchMatches();
